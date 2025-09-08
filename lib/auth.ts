d5d1c59e19af2572679f001340b37137a770f5b6
= <adrianyadav@gmail.com>
2025-09-08 15:06:42 +1000

    test

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,10 +4,16 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL || process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000",
+  trustedOrigins: [
+    process.env.BETTER_AUTH_URL || process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000",
+    "http://localhost:3000", // for local development
+  ],
   emailAndPassword: {
     enabled: true,
   },
   database: prismaAdapter(prisma, {
-    provider: "postgresql", // or "mysql", "postgresql", ...etc
+    provider: "postgresql",
   }),
 });
